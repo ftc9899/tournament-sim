5526b2e860cbd23d84515bc6629780d1813d85ad
ftc9899 <ftc9899@gmail.com>
2019-06-19 18:15:52 -0600

    Add score ceiling and winner-take-all functionality

--- options.py
+++ options.py
@@ -1,3 +1,10 @@
 def init():
 	global ranking_system
 	ranking_system = "current"
+	
+	global score_ceiling
+	score_ceiling = -1
+	
+	# wta_proportion stands for winner-take-all proportion
+	global wta_proportion
+	wta_proportion = None
